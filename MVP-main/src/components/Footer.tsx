27bcac8fb859ddf3304b3d3882b315faa75ac0de
marccenteno8-blip <marccenteno8@gmail.com>
2026-09-17 10:16:45 +0200

    Add files via upload

---
+++ MVP-main/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="ce-footer">
+      <p>
+        Plataforma de reservas para Chic Estilistes (El Masnou). Los datos de estilistas,
+        horario y precios se gestionan en Supabase y pueden editarse ahí en cualquier momento.
+      </p>
+    </footer>
+  );
+}
